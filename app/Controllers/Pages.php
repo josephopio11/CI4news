21089b3240aa92b6833f940981fd111226ad2e97
Joseph Opio <josephopio11@gmail.com>
2020-11-03 20:06:18 +0300

    master

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,4 +6,17 @@ class Pages extends BaseController
     {
         return view('welcome_message');
     }
+
+    public function view($page = 'home')
+    {
+        if (!is_file(APPPATH . '/Views/pages/' . $page . '.php')) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page);
+
+        echo view('templates/header', $data);
+        echo view('pages/' . $page, $data);
+        echo view('templates/footer', $data);
+    }
 }
