75034b2aa69e852eda3b580802f7d121d27434ce
Joseph Opio <josephopio11@gmail.com>
2020-11-03 20:24:58 +0300

    master

--- app/Models/NewsModel.php
+++ app/Models/NewsModel.php
@@ -5,4 +5,15 @@ use CodeIgniter\Model;
 class NewsModel extends Model
 {
     protected $table = 'news';
+    
+    public function getNews($slug = false)
+    {
+        if ($slug === false) {
+            return $this->findAll();
+        }
+
+        return $this->asArray()
+            ->where(['slug' => $slug])
+            ->first();
+    }
 }
